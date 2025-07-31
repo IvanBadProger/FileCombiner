d02a6719cb0afe274eb2094a29cafff99e896440
Van Ivan <programmerivan@mail.ru>
2025-08-01 00:01:11 +0700

    fix: build.py

--- scripts/build.py
+++ scripts/build.py
@@ -2,12 +2,12 @@ from PyInstaller.__main__ import run
 
 run(
     [
-        "main.py",
+        "src/main.py",
         "--onefile",
         "--windowed",
         "--icon=assets/icon.ico",
         "--name=FileCombiner",
-        "--add-data=core;core",
-        "--add-data=ui;ui",
+        "--add-data=src/core;core",
+        "--add-data=src/ui;ui",
     ]
 )
